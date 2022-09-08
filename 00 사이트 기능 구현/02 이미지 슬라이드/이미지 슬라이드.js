d772262666fde5ffaf0fb43ee13cf0ff092326ee
threehHong <honghyohun@naver.com>
2022-09-08 20:44:42 +0900

    사이트 기능 구현

---
+++ 00 사이트 기능 구현/02 이미지 슬라이드/이미지 슬라이드.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let btn = document.querySelector(".btn");
+    btn.onclick = function(){
+        btn.style.color = "blue";
+    }
+    
+})

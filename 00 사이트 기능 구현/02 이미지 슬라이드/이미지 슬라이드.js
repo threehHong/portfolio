f3d3b1d57be66ffef57357f5844ce5b19fb0bf54
threehHong <honghyohun@naver.com>
2022-09-23 21:08:54 +0900

    portfolio

--- 00 사이트 기능 구현/02 이미지 슬라이드/이미지 슬라이드.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let btn = document.querySelector(".btn");
-    btn.onclick = function(){
-        btn.style.color = "blue";
-    }
-    
-})
